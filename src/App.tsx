426522049988708faff30b4e87ccf68c29d87919
sagarmj94 <sagarmj1502@gmail.com>
2026-02-22 16:29:47 +0530

    ForwardRef method learn in this branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import { useRef } from "react";
 import Button from "./components/Button";
 import Container from "./components/Container";
 import Input from "./components/Inputs";
 
 function App() {
-  const inputRef = useRef(null);
-
   return (
     <main>
       <Container as={Button} onClick={() => {}}>
         Click Me
       </Container>
-      <Input label="Test" id="test" ref={inputRef} />
+
       {/* <Input id="name" label="Your name" type="text" />
       <Input id="age" label="Your age" type="number" />
 
--- src/components/Inputs.tsx
+++ src/components/Inputs.tsx
@@ -1,25 +1,15 @@
-import {
-  ComponentPropsWithoutRef,
-  ComponentPropsWithRef,
-  forwardRef,
-} from "react";
+import { ComponentPropsWithoutRef } from "react";
 
 type InputProps = {
   label: string;
   id: string;
-  ref: string;
 } & ComponentPropsWithoutRef<"input">;
 
-const Input = forwardRef<HTMLInputElement, InputProps>(function Input(
-  { label, id, ...props },
-  ref,
-) {
+export default function Input({ label, id, ...props }: InputProps) {
   return (
     <p>
       <label htmlFor={id}>{label}</label>
-      <input type="text" id={id} {...props} ref={ref} />
+      <input type="text" id={id} {...props} />
     </p>
   );
-});
-
-export default Input;
+}
